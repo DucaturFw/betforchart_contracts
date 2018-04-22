9665ec21237e838a7a9e15adc86dc671f6396230
devmanio <ivan.kocheshev@gmail.com>
2018-04-22 14:07:57 +0300

    blueprint version

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -15,31 +15,53 @@ pragma solidity ^0.4.18;
 contract Bet {
 	
     address public owner;
+    address public opponent;
     uint256 public rate;
+    uint256 public minanswer;
+    uint256 public predictrate;
+    enum Stage {bet,answer,rag}
+    Stage stage;
 
     modifier onlyOracle() {
 	if(oracle!=msg.sender) revert();
         _;	
 
     }
-    function Bet(uint256 amount) {
-	oracle = ''
+
+    function Bet(uint256 _minanswer, address _oracle, uint256 _predictrate ) {
+	    oracle = _oracle
+        minanswer = _minanswer
+        predictrate = _predictrate
+        stage = Stage.bet;
+        owner = msg.sender
+
         //get main bet
         
     }
     
-    function Answer() {
+    function Answer(uint256 _amount) {
+        require(_amount > _minanswer );
+        opponent = msg.sender;
+        stage = Stage.answer;
         //get answer bet
         
     }
     
     function getWon() {
+        require(stage == Stage.rag); 
         // decide to won
+        if (predictrate > rate && owner == msg.sender ) {
+            owner.transfer(this.balance);
+        } else {
+            opponent.transfer(this.balance);
+        }
         
     }
 
    function _(uint256 _rate) onlyOracle {
 	rate = _rate;
+    stage = Stage.rag;
+
    }
 }    
 
