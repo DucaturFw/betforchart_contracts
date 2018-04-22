95d3995df65e0bcf0ed9487d73606e5e8fa962f7
devmanio <ivan.kocheshev@gmail.com>
2018-04-22 12:09:12 +0300

    draft

---
+++ contracts/bet.sol
@@ -0,0 +1,32 @@
+
+pragma solidity ^0.4.18;
+
+/**
+ * Copyright (C) 2018  Ducatur, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
+ */
+    
+contract Bet {
+    function Bet(uint256 amount) {
+        //get main bet
+        
+    }
+    
+    function Answer() {
+        //get answer bet
+        
+    }
+    
+    function getWon() {
+        // decide to won
+        
+    }
+}    
+
+
